0f8197d90f22658a3ca20b59d68ce3acc18e5ba9
Marcelo Benjamin <marcelo.dfx@gmail.com>
2019-03-01 12:15:29 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/benjamin/springbootmc/dto/CategoriaDTO.java
@@ -0,0 +1,36 @@
+package com.benjamin.springbootmc.dto;
+
+import java.io.Serializable;
+
+import com.benjamin.springbootmc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
