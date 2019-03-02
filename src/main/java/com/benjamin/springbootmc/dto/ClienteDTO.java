3c95d099f6cfc9ea9de000ceea60c1c61ab1d221
Marcelo Benjamin <marcelo.dfx@gmail.com>
2019-03-02 12:47:34 -0300

    Validação costomizada: email não repetido na atualização de cliente

--- src/main/java/com/benjamin/springbootmc/dto/ClienteDTO.java
+++ src/main/java/com/benjamin/springbootmc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.benjamin.springbootmc.domain.Cliente;
+import com.benjamin.springbootmc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
